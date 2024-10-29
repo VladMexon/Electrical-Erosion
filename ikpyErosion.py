b4ffa4c6b9fd382daff1588d26fadfaafb288e0d
VladMexon <78736104+VladMexon@users.noreply.github.com>
2024-10-29 21:09:21 +0500

    urdf corrected

--- ikpyErosion.py
+++ ikpyErosion.py
@@ -1,5 +1,7 @@
-import ikpy.chain as chain
-import ikpy.link as link
+import ikpy.chain
+import numpy as np
 
+my_chain = ikpy.chain.Chain.from_urdf_file("3DModel/urdf/unnamed.urdf")
 
-
+target_position = [ 4, 4, 5]
+print("The angles of each joints are : ", my_chain.inverse_kinematics(target_position))
